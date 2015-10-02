bf59b7d2d19a25056da006778dfe7c474866ffd5
Justin Mayfield <tooker@gmail.com>
2015-10-02 02:31:40 -0600

    Fix version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def long_desc():
 
 setup(
     name='shellish',
-    version='0.7.0',
+    version='0.8.0',
     description='A framework for CLI/shell programs.',
     author='Justin Mayfield',
     author_email='tooker@gmail.com',
