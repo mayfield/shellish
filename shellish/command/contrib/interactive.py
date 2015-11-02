764c0c3512fb9755ea950266755dd3d0f72d7d05
Justin Mayfield <tooker@gmail.com>
2015-11-02 16:58:04 -0700

    Add some tty and pager mgmt contrib commands.
    With the advent of paging we could use some contrib commands to make
    working with them better.  The reset command just sends the vt100 reset
    control for cases where things get screwed up.

--- shellish/command/contrib/interactive.py
+++ shellish/command/contrib/interactive.py
@@ -62,3 +62,40 @@ class Exit(command.Command):
 
     def run(self, args):
         self.session.exit()
+
+
+class Reset(command.Command):
+    """ Reset the TTY. """
+
+    name = 'reset'
+
+    def run(self, args):
+        print('\033c')
+
+
+class Pager(command.Command):
+    """ Enable or disable the pager for commands. """
+
+    name = 'pager'
+
+    def enable(self, args):
+        if self.session.allow_pager:
+            raise SystemExit("Already enabled")
+        self.session.allow_pager = True
+
+    def disable(self, args):
+        if not self.session.allow_pager:
+            raise SystemExit("Already disabled")
+        self.session.allow_pager = False
+
+    def setup_args(self, parser):
+        self.add_subcommand(command.Command(name='enable', run=self.enable))
+        self.add_subcommand(command.Command(name='disable', run=self.disable))
+
+    def run(self, args):
+        if self.session.allow_pager:
+            self.session.allow_pager = False
+            print("Disabled pager")
+        else:
+            self.session.allow_pager = True
+            print("Enabled pager: %s" % self.get_config('core').get('pager'))
