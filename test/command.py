c33d9a6fa4717a55a0d7cce716b41feda2d77464
Justin Mayfield <tooker@gmail.com>
2015-11-02 17:00:41 -0700

    Unittest for full Command composition.

--- test/command.py
+++ test/command.py
@@ -43,3 +43,40 @@ class TestNesting(unittest.TestCase):
         a.add_subcommand(B, default=True)
         self.assertEqual(a(argv=''), 'B')
         self.assertEqual(a(argv='b'), 'B')
+
+
+class CommandCompose(unittest.TestCase):
+
+    def test_naked(self):
+        a = shellish.Command()
+        self.assertRaises(SystemExit, a, argv='')
+
+    def test_onlyrun(self):
+        ok = object()
+        a = shellish.Command(run=lambda _: ok)
+        self.assertIs(a(argv=''), ok)
+
+    def test_allrunners(self):
+        refcnt = 0
+        def runner(args, **ign):
+            nonlocal refcnt
+            refcnt += 1
+        a = shellish.Command(prerun=runner, run=runner, postrun=runner)
+        a(argv='')
+        self.assertEqual(refcnt, 3)
+
+    def test_subcommand_nameless(self):
+        a = shellish.Command()
+        b = shellish.Command()
+        self.assertRaises(TypeError, a.add_subcommand, b)
+
+    def test_subcommand_name_after_init(self):
+        a = shellish.Command()
+        b = shellish.Command()
+        b.name = 'okay'
+        a.add_subcommand(b)
+
+    def test_subcommand_name_at_construct(self):
+        a = shellish.Command()
+        b = shellish.Command(name='okay')
+        a.add_subcommand(b)
