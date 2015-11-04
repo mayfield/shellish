6ac809a635e81fed211e375c272d0bb741925790
Justin Mayfield <tooker@gmail.com>
2015-11-04 15:35:22 -0700

    Non numeric values in version was bad.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def long_desc():
 
 setup(
     name='shellish',
-    version='2.x',
+    version='2.1',
     description='A framework for CLI/shell programs.',
     author='Justin Mayfield',
     author_email='tooker@gmail.com',
