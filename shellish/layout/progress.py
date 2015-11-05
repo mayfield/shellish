cb2fb4dc704437d28f439cec2469bb34518e849e
Justin Mayfield <tooker@gmail.com>
2015-11-05 10:59:41 -0700

    Make progress bar a conext manager.

--- shellish/layout/progress.py
+++ shellish/layout/progress.py
@@ -9,17 +9,20 @@ from . import vtml
 
 
 class ProgressBar(object):
-    """ Progress bar class for manual control. """
+    """ Progress bar class for more advanced control apps.  Most likely this
+    should just be used by subclasses or by the generator functions. """
 
     partials = vtml.beststr('▏▎▎▍▍▌▌▋▋▊▊▉▉█', '#')
     fill = vtml.beststr('▯', '-')
 
     def __init__(self, min=0, max=1, width=None, file=None, prefix=' ',
-                 suffix=' ', bar_style='', fill_style='', show_percent=True):
+                 suffix=' ', bar_style='', fill_style='', show_percent=True,
+                 clear=False):
         if width is None:
             width = shutil.get_terminal_size()[0]
         if file is None:
             self.file = sys.stderr
+        self.clear_on_exit = clear
         self.min_value = min
         self.max_value = max
         self.width = width
@@ -31,6 +34,17 @@ class ProgressBar(object):
         self.bar_style = bar_style
         self.fill_style = fill_style
         self.show_percent = show_percent
+
+    def __enter__(self):
+        self.hide_cursor()
+        return self
+
+    def __exit__(self, *exc):
+        if self.clear_on_exit:
+            self.clear()
+        else:
+            print(file=self.file)
+        self.restore_cursor()
 
     def draw(self):
         pct = (self.value - self.min_value) / self.max_value
@@ -55,22 +69,24 @@ class ProgressBar(object):
     def clear(self):
         vtml.vtmlprint('\r', ' ' * self.width, file=self.file)
 
+    def hide_cursor(self):
+        print('\033[?25l', file=self.file)
 
-def progressbar(stream, clear=False, prefix='Loading: ', width=0.5, **options):
+    def restore_cursor(self):
+        print('\033[?25h', file=self.file)
+
+
+def progressbar(stream, prefix='Loading: ', width=0.5, **options):
     """ Generator filter to print a progress bar. """
     size = len(stream)
     if not size:
         return stream
     if 'width' not in options:
-        if width < 1:
+        if width <= 1:
             width = round(shutil.get_terminal_size()[0] * width)
         options['width'] = width
-    b = ProgressBar(max=size, prefix=prefix, **options)
-    b.set(0)
-    for i, x in enumerate(stream, 1):
-        b.set(i)
-        yield x
-    if clear:
-        b.clear()
-    else:
-        print()
+    with ProgressBar(max=size, prefix=prefix, **options) as b:
+        b.set(0)
+        for i, x in enumerate(stream, 1):
+            yield x
+            b.set(i)
