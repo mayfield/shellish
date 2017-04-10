665669994bfbc0f0549f53ac34566f51b7492d4d
Justin Mayfield <tooker@gmail.com>
2017-04-10 09:21:58 -0600

    Help command improvements.
     1. Support for multiple command args to `help`, a la
            `help command subcommand subsubcommand`
     2. Much better tab completion.
     3. Find similarly named commands when the command can't be found in
        case the user made a typing/spelling error.

--- shellish/command/contrib/help.py
+++ shellish/command/contrib/help.py
@@ -2,7 +2,10 @@
 Alternate form of getting help.
 """
 
+import difflib
+import sys
 from .. import command
+from ... import rendering
 
 
 class Help(command.Command):
@@ -12,32 +15,43 @@ class Help(command.Command):
     use_pager = True
 
     def setup_args(self, parser):
-        self.add_argument('command', nargs='?', complete=self.command_choices,
-                          help='Command name to show help for.')
+        self.add_argument('command', nargs='*', complete=self.command_choices,
+                          help='Command name to show help of.')
 
     def command_choices(self, prefix, args):
-        return frozenset(x for x in self.parent.subcommands
-                         if x.startswith(prefix))
+        if args.command:
+            cmd = self.find_command(args.command[:-1 if prefix else None])
+        else:
+            cmd = self.find_root()
+        return frozenset(x for x in cmd.subcommands if x.startswith(prefix))
+
+    def stderr(self, *args, **kwargs):
+        return rendering.vtmlprint(*args, file=sys.stderr, **kwargs)
+
+    def find_command(self, path):
+        cmdptr = self.find_root()
+        for i, cmd in enumerate(path):
+            try:
+                cmdptr = cmdptr.subcommands[cmd]
+            except KeyError:
+                raise LookupError(cmdptr, i)
+        return cmdptr
 
     def run(self, args):
         if not args.command:
-            self.print_overview()
+            self.find_root().argparser.print_help()
+            return
+        try:
+            cmd = self.find_command(args.command)
+        except LookupError as e:
+            good_cmd, fail_idx = e.args
+            good = ' '.join(args.command[:fail_idx]) + ' ' if fail_idx else ''
+            bad = ' '.join(args.command[fail_idx:])
+            self.stderr('<red>Command not found:</red> %s<b><red>%s' % (good,
+                        bad))
+            for x in difflib.get_close_matches(args.command[fail_idx],
+                                               good_cmd.subcommands):
+                self.stderr('    Did you mean: %s<b>%s</b>?' % (good, x))
+            raise SystemExit(1)
         else:
-            try:
-                command = self.parent.subcommands[args.command]
-            except KeyError:
-                raise SystemExit("Invalid command")
-            command.argparser.print_help()
-
-    def print_overview(self):
-        ap = self.find_root().argparser
-        formatter = ap._get_formatter()
-        formatter.add_text(ap.description)
-        for x in ap._action_groups:
-            if x.title == 'subcommands':
-                formatter.start_section(x.title)
-                formatter.add_text(x.description)
-                formatter.add_arguments(x._group_actions)
-                formatter.end_section()
-                break
-        print(formatter.format_help())
+            cmd.argparser.print_help()
