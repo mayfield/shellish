d455094a6b447d0ce9d5b6602fc9ca95047895c6
Justin Mayfield <tooker@gmail.com>
2017-04-10 12:45:40 -0600

    Remove python3.4 ref in setup.py

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries',
         'Topic :: System :: Shells',
     ]
